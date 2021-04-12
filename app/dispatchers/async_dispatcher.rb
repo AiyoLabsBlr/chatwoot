861a3e91a0ee66cc293fd18f54f866cc243ab14a
Aekansh Partani <aekanshpartani@gmail.com>
2021-04-12 05:56:36 -0400

    Postback Button Webhook Fixes

--- app/dispatchers/async_dispatcher.rb
+++ app/dispatchers/async_dispatcher.rb
@@ -1,6 +1,10 @@
 class AsyncDispatcher < BaseDispatcher
   def dispatch(event_name, timestamp, data)
-    EventDispatcherJob.perform_later(event_name, timestamp, data)
+    if event_name == 'message.updated'
+      EventDispatcherJob.perform_now(event_name, timestamp, data)
+    else
+      EventDispatcherJob.perform_later(event_name, timestamp, data)
+    end
   end
 
   def publish_event(event_name, timestamp, data)
