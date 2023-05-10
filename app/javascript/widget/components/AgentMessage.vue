de109d0783de5b507cd47a48ff99355f057236ff
Anish <anish@aiyolabs.com>
2023-05-10 16:04:16 +0530

    hiding agent name

--- app/javascript/widget/components/AgentMessage.vue
+++ app/javascript/widget/components/AgentMessage.vue
@@ -223,6 +223,7 @@ export default {
     font-size: $font-size-small;
     font-weight: $font-weight-medium;
     margin: $space-small 0;
+    display: none !important;
     padding-left: $space-micro;
   }
 
